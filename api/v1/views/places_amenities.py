d8279941155f547465ac534cb59ccd43b194cf5d
ahmedtiko <ahmedmm_tarek@hotmail.com>
2024-05-26 18:37:56 +0000

    advanced #1

---
+++ api/v1/views/places_amenities.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""
+Place-Amenity relationship view
+"""
+
+from flask import jsonify, abort, request
+from api.v1.views import app_views
+from models import storage
+from models.place import Place
+from models.amenity import Amenity
+from os import getenv
+
+STORAGE_TYPE = getenv('HBNB_TYPE_STORAGE')
+
+
+@app_views.route('/places/<place_id>/amenities', methods=['GET'], strict_slashes=False)
+def get_place_amenities(place_id):
+    """ Retrieves the list of all Amenity objects of a Place """
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404)
+    if STORAGE_TYPE == 'db':
+        amenities = place.amenities
+    else:
+        amenities = [storage.get(Amenity, amenity_id) for amenity_id in place.amenity_ids]
+    return jsonify([amenity.to_dict() for amenity in amenities])
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+def delete_place_amenity(place_id, amenity_id):
+    """ Deletes a Amenity object from a Place """
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404)
+    amenity = storage.get(Amenity, amenity_id)
+    if not amenity:
+        abort(404)
+    if STORAGE_TYPE == 'db':
+        if amenity not in place.amenities:
+            abort(404)
+        place.amenities.remove(amenity)
+    else:
+        if amenity_id not in place.amenity_ids:
+            abort(404)
+        place.amenity_ids.remove(amenity_id)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'], strict_slashes=False)
+def link_place_amenity(place_id, amenity_id):
+    """ Links a Amenity object to a Place """
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404)
+    amenity = storage.get(Amenity, amenity_id)
+    if not amenity:
+        abort(404)
+    if STORAGE_TYPE == 'db':
+        if amenity in place.amenities:
+            return jsonify(amenity.to_dict()), 200
+        place.amenities.append(amenity)
+    else:
+        if amenity_id in place.amenity_ids:
+            return jsonify(amenity.to_dict()), 200
+        place.amenity_ids.append(amenity_id)
+    storage.save()
+    return jsonify(amenity.to_dict()), 201
